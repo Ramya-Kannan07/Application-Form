e1c08cdf206fdcdb4c06d75929afb4b6e7544801
Ramya-Kannan07 <ramyakannan085@gmail.com>
2024-09-09 15:51:06 +0530

    footer content changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Application Management App. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Application Management App. All rights reserved by Ramya</p>
     </footer>
   );
 };
